982b51ae9657d19af16d85f4fe3d6b3697cfc4e0
Tgoduiem <vn.toocn@gmail.com>
2024-11-05 02:02:58 +0700

    Update Mainnew.lua

--- Mainnew.lua
+++ Mainnew.lua
@@ -92,9 +92,6 @@ Main.MouseButton1Click:Connect(function()
 UICorner.CornerRadius = UDim.new(0, 12)
 UICorner.Parent = Main
 
-    scaleUpTween:Play()
-    scaleUpTween.Completed:Connect(function()
-        scaleDownTween:Play()
     end)
 end)
 
